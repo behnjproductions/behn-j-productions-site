a118e04b3e227b41b685f0c33967bd33e580503c
behnjproductions <behnjedy@gmail.com>
2026-09-13 16:21:27 -0400

    Refactor rendering logic to include AProposPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import { AProposPage } from "./AProposPage.jsx";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const path = window.location.pathname.replace(/\/+$/, "") || "/";
+const RootComponent = path === "/a-propos" ? AProposPage : App;
+
+createRoot(document.getElementById("root")).render(<RootComponent />);

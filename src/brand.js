04e55a69914090d8c0103a361ff4a9016f6bcdfd
behnjproductions <behnjedy@gmail.com>
2026-09-13 16:19:37 -0400

    Add brand information to brand.js

---
+++ src/brand.js
@@ -0,0 +1,8 @@
+export const BRAND = {
+    email: 'contact@behnjphoto.com',
+    phone: '514 458-4730',
+    phoneHref: 'tel:+15144584730',
+    schoolPortal: 'https://behnjphotos.ca',
+    facebook: 'https://www.facebook.com/share/1P2sLfxUaz/?mibextid=wwXIfr',
+    instagram: 'https://www.instagram.com/behnjproductions',
+};
